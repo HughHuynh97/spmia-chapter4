8316205c05d825076412db005a96d7803408cae6
hugh.huynh <hugh.huynh@pal.net.vn>
2024-10-31 16:27:28 +0700

    # Update complete connect to db

---
+++ organization-service/src/main/java/com/thoughtmechanix/controller/OrganizationServiceController.java
@@ -0,0 +1,36 @@
+package com.thoughtmechanix.controller;
+
+import com.thoughtmechanix.model.Organization;
+import com.thoughtmechanix.service.OrganizationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "v1/organizations")
+public class OrganizationServiceController {
+    @Autowired
+    private OrganizationService orgService;
+
+
+    @GetMapping(value = "/{organizationId}")
+    public Organization getOrganization(@PathVariable("organizationId") String organizationId) {
+        return orgService.getOrg(organizationId);
+    }
+
+    @PutMapping(value = "/{organizationId}")
+    public void updateOrganization(@PathVariable("organizationId") String orgId, @RequestBody Organization org) {
+        orgService.updateOrg(org);
+    }
+
+    @PostMapping(value = "/{organizationId}")
+    public void saveOrganization(@RequestBody Organization org) {
+        orgService.saveOrg(org);
+    }
+
+    @DeleteMapping(value = "/{organizationId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteOrganization(@PathVariable("orgId") String orgId, @RequestBody Organization org) {
+        orgService.deleteOrg(org);
+    }
+}
